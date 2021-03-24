2587d316b69e51d3a6d6237080c576d926e69525
naglenda31 <naglenda31@gmail.com>
2021-03-24 02:55:20 +0000

    added jumbotron

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { MyNav } from "./navbar";
+import { MyJumbotron } from "./jumbotron";
 
 //create your first component
 export function Home() {
@@ -12,6 +13,12 @@ export function Home() {
 	return (
 		<div>
 			<MyNav menu={menuLinks} brand="Start Bootstrap" />
+			<MyJumbotron
+				title="A Warm Welcome!"
+				description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla fermentum lacinia. Aliquam turpis leo, interdum quis lectus et, vestibulum cursus lectus."
+				buttonURL="/"
+				buttonLabel="Call to action!"
+			/>
 		</div>
 	);
 }
--- src/js/component/jumbotron.js
+++ src/js/component/jumbotron.js
@@ -1,18 +1,27 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 
 //create your first component
-export function MyJumbotron() {
-    const renderJumbotron = jumboTitle(h1) => {
-
-    }
+export function MyJumbotron(props) {
 	return (
-	<div className="jumbotron">
-        <h1 className="display-4">Hello, world!</h1>
-        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-        <hr className="my-4"/>
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-    </div>
+		<div className="container-fluid my-4">
+			<div className="jumbotron mx-3">
+				<h1 className="display-1">{props.title}</h1>
+				<p className="lead">{props.description}</p>
+				<a
+					className="btn btn-primary btn-lg"
+					href={props.buttonURL}
+					role="button">
+					{props.buttonLabel}
+				</a>
+			</div>
+		</div>
 	);
 }
+
+MyJumbotron.propTypes = {
+	title: PropTypes.string,
+	description: PropTypes.string,
+	buttonURL: PropTypes.string,
+	buttonLabel: PropTypes.string
+};
